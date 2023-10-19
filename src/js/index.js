1a72433feabd81f2bfce925ed151304a0bb877f4
Artem <krylov.artem.1995@gmail.com>
2023-10-19 20:01:59 +0200

    add style in css

--- src/js/index.js
+++ src/js/index.js
@@ -57,7 +57,7 @@ function markup(data) {
   const { breeds, url } = data[0];
   const { name, temperament, description } = breeds[0];
   const catList = `<img src="${url}" alt="${name}" width=500>
-  <div>
+  <div class ="back-color">
 <h2 class="title">${name}</h2>
 <p class="text">${description}</p>
 <p class="text span-text"><span class="span">Temperament:</span> ${temperament}</p>
